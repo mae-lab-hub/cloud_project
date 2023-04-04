521c57dc50990ebfd90fedf8d1bfd71c0206c2b5
@mae-lab-hub <asmaeallou@outlook.com>
2023-04-04 01:59:13 -0400

    Saving the diffrent entities and sending them to the front end

--- Capabilities/app.py
+++ Capabilities/app.py
@@ -33,11 +33,52 @@ def upload_image():
     image_info = storage_service.upload_file(file_bytes, file_name)
     text_lines = recognition_service.detect_text(file_name)
 
+    #since the api can detect multiple of the same entities, we will add them all to a list
+
+    name = []
+    email = []
+    address = []
+    phone=[]
+    website =[]
+
+
     for line in text_lines:
+
         print("TEXT:", line['text'])
         entity = comprehend_service.detect_entities(line['text'])
-        print("ENTITY: ", entity)
+
+        for en in entity:
+            print("ENTITY: ", en['Type'])
+
+            if en['Type'] == "NAME":
+                name.append(en['Text'])
+            
+            elif en['Type'] == "EMAIL":
+                email.append(en['Text'])   
+            
+            elif en['Type'] == "ADDRESS":
+                address.append(en['Text'])
+
+            elif en['Type'] == "PHONE_OR_FAX":
+                phone.append(en['Text'])
+            
+            elif en['Type'] == "URL":
+                website.append(en['Text'])
+        
+
+    #for now we just join all the same entities together and the user can remove the one they want
+    #possible changes: each of the same entity is an option in a dropdown menu, but then if the user wants both entities saved together it wil be complicated to edit, example: New York and Florence st.
     
+    name = "  ".join(name)
+    email  = " ".join(email)
+    phone = " ".join(phone)
+    address = " ".join(address)
+    website = " ".join(website)
 
+    entity_data = { "name":name,
+                    "email":email,
+                    "address":address,
+                    "phone":phone,
+                    "website":website}
 
-    return image_info
+    return image_info , entity_data
