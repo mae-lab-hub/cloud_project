89a443bf6e3f2eeadb3fd61ebf1db7a971e12460
@mae-lab-hub <asmaeallou@outlook.com>
2023-04-04 00:51:40 -0400

    Added medical comprehend

--- Capabilities/app.py
+++ Capabilities/app.py
@@ -3,7 +3,7 @@ from chalicelib import storage_service
 from chalicelib import recognition_service
 import base64
 import json
-
+from chalicelib import comprehend_service
 
 
 
@@ -19,7 +19,7 @@ app.debug = True
 storage_location = 'contentcen301159608.aws.ai'
 storage_service = storage_service.StorageService(storage_location)
 recognition_service = recognition_service.RecognitionService(storage_service)
-
+comprehend_service = comprehend_service.ComprehendDetect()
 #####
 # RESTful endpoints
 #####
@@ -31,9 +31,13 @@ def upload_image():
     file_bytes = base64.b64decode(request_data['filebytes'])
 
     image_info = storage_service.upload_file(file_bytes, file_name)
+    text_lines = recognition_service.detect_text(file_name)
+
+    for line in text_lines:
+        print("TEXT:", line['text'])
+        entity = comprehend_service.detect_entities(line['text'])
+        print("ENTITY: ", entity)
+    
+
 
     return image_info
-
-
-
- 
