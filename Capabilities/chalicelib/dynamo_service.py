8a26b3880dcfc76ec7bf187e8ff89b28031b8972
@mae-lab-hub <asmaeallou@outlook.com>
2023-04-14 14:33:21 -0400

    added basic commands for adding deleting  updating and searching

--- Capabilities/chalicelib/dynamo_service.py
+++ Capabilities/chalicelib/dynamo_service.py
@@ -1,5 +1,5 @@
 import boto3
-
+from boto3.dynamodb.conditions import Key
 class DynamoService:
     def __init__(self):
         dynamodb = boto3.resource('dynamodb')
@@ -7,29 +7,26 @@ class DynamoService:
 
     def save_item(self,data):
 
-        print('hiuhi')
-
         response = self.table.put_item(
         Item=data)
-
-
-
-
-
-       #response = self.table.put_item(
-       # Item={
-        #    'lead_name': 'Canada',
-         #   'creation_date': '04-14-2023',
-          #  'address':"100 CLoucester",
-           # 'email':'email@email.com',
-            #'phone_number':'666-999-666',
-           # 'url':'www.site.com'
-
-        #})
-
-        print("sucess??")
     
+    
+    def delete_contact(self, lead_name):
 
+            self.table.delete_item(Key={'lead_name': lead_name, 'creation_date':'04-14-2023'})
+        
+    def update_contact(self, data):
 
+        response = self.table.update_item(
+                        Key={'lead_name': data['lead_name'], 'creation_date': data['creation_date']},
+                        UpdateExpression="set address=:a, phone_number=:p, site=:u,email=:e",
+                        ExpressionAttributeValues={
+                            ':a': data['address'], ':p': data['phone_number'],':u':data['site'],':e':data['email']},
+                        ReturnValues="UPDATED_NEW")
 
+    def query_contacts(self, lead_name):
 
+        response = self.table.query(KeyConditionExpression=Key("lead_name").eq(lead_name))
+        
+        items = response['Items']
+        print(items)
